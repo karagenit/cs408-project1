ccb72df022bde07f51584f80f067e0207d0fad3d
Caleb Smith <karagenit@outlook.com>
2020-02-08 20:11:41 -0500

    Implement addNode

--- proj1/q4/CFG.java
+++ proj1/q4/CFG.java
@@ -11,33 +11,46 @@ public class CFG {
     Map<Node, Set<Node>> edges = new HashMap<Node, Set<Node>>();
 
     static class Node {
-	int position;
-	MethodNode method;
-	ClassNode clazz;
+        int position;
+        MethodNode method;
+        ClassNode clazz;
 
-	Node(int p, MethodNode m, ClassNode c) {
-	    position = p; method = m; clazz = c;
-	}
+        Node(int p, MethodNode m, ClassNode c) {
+            position = p; method = m; clazz = c;
+        }
 
-	public boolean equals(Object o) {
-	    if (!(o instanceof Node)) return false;
-	    Node n = (Node)o;
-	    return (position == n.position) &&
-		method.equals(n.method) && clazz.equals(n.clazz);
-	}
+        public boolean equals(Object o) {
+            if (!(o instanceof Node)) return false;
+            Node n = (Node)o;
+            return (position == n.position) &&
+            method.equals(n.method) && clazz.equals(n.clazz);
+        }
 
-	public int hashCode() {
-	    return position + method.hashCode() + clazz.hashCode();
-	}
+        public int hashCode() {
+            return position + method.hashCode() + clazz.hashCode();
+        }
 
-	public String toString() {
-	    return clazz.name + "." +
-		method.name + method.signature + ": " + position;
-	}
+        public String toString() {
+	        return clazz.name + "." +
+	        method.name + method.signature + ": " + position;
+        }
     }
 
+    /*
+     * Inserts a new node.
+     * If the node doesn't already exist in the set of nodes,
+     * insert the node into the set of nodes and associate
+     * an empty set of nodes as the value of the new node
+     * in the map of edges.
+     */
     public void addNode(int p, MethodNode m, ClassNode c) {
-	// ...
+	    Node n = new Node(p, m, c);
+        Set<Node> connections = new HashSet<Node>();
+
+        if (!nodes.contains(n)) {
+            nodes.add(n);
+            edges.put(n, connections);
+        }
     }
 
     public void addEdge(int p1, MethodNode m1, ClassNode c1,
